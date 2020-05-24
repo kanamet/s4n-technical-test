df514495a4c48b2d6fe137bd83d8993da9477e35
Johan D. Torres <ingeniero204@gmail.com>
2020-05-24 10:33:29 -0500

    Initial Commit

---
+++ src/main/java/com/corrientazo/domicilios/Main.java
@@ -0,0 +1,7 @@
+package com.corrientazo.domicilios;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Deliveries - Su corrientazo a domicilio");
+    }
+}
